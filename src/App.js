f81c6dc916b4a531eabbf1e63cfe79693fc3146d
yoimaral <yalozano9@misena.edu.co>
2021-02-12 18:05:45 -0500

    The type and data query of the pokemon is generated

--- src/App.js
+++ src/App.js
@@ -11,31 +11,60 @@ const [pokemonType, setPokemonType] = useState("");
 const getPokemon = async () => {
   const toArray = [];
   try {
-  const url = "https://pokeapi.co/api/v2/pokemon/${pokemon}"
-  const res = await axios.get(url)
-  console.log(res)
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+  const res = await axios.get(url);
+  toArray.push(res.data);
+  setPokemonType(res.data.types[0].type.name)
+  setPokemonData(toArray);
+  console.log(res);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 const handleChange = (e) => {
-  setPokemon(e.target.value.toLowerCase())
-}
+  setPokemon(e.target.value.toLowerCase());
+};
 
 const handleSubmit = (e) => {
 e.preventDefault();
-getPokemon()
-}
+getPokemon();
+};
 
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <label>
-<input type="text" onChange={handleChange} placeholder="Enter Pokemon Name" />
-          </label>
-        </form>
+          <input type="text" onChange={handleChange} placeholder="Enter Pokemon Name" />
+         </label>
+      </form>
+        {pokemonData.map((data) => {
+          return(
+            <div className="container">
+              <img src="" alt=""/>
+              <div className="divtable">
+                <div className="divtableBody"></div>
+                <div className="divtableRow">
+                  <div className="divtableCell">Type</div>
+                  <div className="divtableCell">{pokemonType}</div>
+                </div>
+                <div className="divtableRow">
+                  <div className="divtableCell">Type</div>
+                  <div className="divtableCell">{""}{Math.round(data.height * 3.9)}</div>
+                </div>
+                <div className="divtableRow">
+                  <div className="divtableCell">weight</div>
+                  <div className="divtableCell">{" "} {Math.round(data.weight / 4.3)} lbs</div>
+                </div>
+                <div className="divtableRow">
+                  <div className="divtableCell">Type</div>
+                  <div className="divtableCell">{pokemonType}</div>
+                </div>
+              </div>
+            </div>
+          )
+        })}
     </div>
   );
 };
