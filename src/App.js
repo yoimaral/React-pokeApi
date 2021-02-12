a5b178649b5a439f0a8024ca3bfb1c94ae8e1b60
yoimaral <yalozano9@misena.edu.co>
2021-02-12 11:09:12 -0500

    Feat: The get endpoint is implemented to capture the pokemon. The name of the pokemon is sent through the form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
+import axios from "axios";
 import './App.css';
 
-function App() {
+const App = () => {
+
+const [pokemon, setPokemon] = useState("pikachu");
+const [pokemonData, setPokemonData] = useState([]);
+const [pokemonType, setPokemonType] = useState("");
+
+const getPokemon = async () => {
+  const toArray = [];
+  try {
+  const url = "https://pokeapi.co/api/v2/pokemon/${pokemon}"
+  const res = await axios.get(url)
+  console.log(res)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const handleChange = (e) => {
+  setPokemon(e.target.value.toLowerCase())
+}
+
+const handleSubmit = (e) => {
+e.preventDefault();
+getPokemon()
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+<input type="text" onChange={handleChange} placeholder="Enter Pokemon Name" />
+          </label>
+        </form>
     </div>
   );
-}
+};
 
 export default App;
